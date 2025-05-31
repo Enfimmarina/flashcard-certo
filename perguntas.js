e37ab8a8807c18bc739b48ba03d45041f8f5f093
Enfimmarina <marina.siqueira.silva@escola.pr.gov.br>
2025-05-31 14:00:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Inglês',
+    'What does CAT mean?',
+    'Gato'
+)
+
+criaCartao(
+    'Inglês',
+    'What does CHICKEN mean?',
+    'Galinha'
+)
+
+criaCartao(
+    'Inglês',
+    'What does COW mean?',
+    'Vaca'
+)
+
+criaCartao(
+    'Inglês',
+    'What does DOG mean?',
+    'Cachorro'
+)
+
+criaCartao(
+    'Inglês',
+    'What does PORK mean?',
+    'Porco'
+)
