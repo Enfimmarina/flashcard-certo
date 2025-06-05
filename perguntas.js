fd9b8707fed010afc1e94729a4556d5a7c033a25
Enfimmarina <marina.siqueira.silva@escola.pr.gov.br>
2025-06-05 14:24:26 +0000

    coloquei mais cartões

--- perguntas.js
+++ perguntas.js
@@ -27,3 +27,27 @@ criaCartao(
     'What does PORK mean?',
     'Porco'
 )
+
+criaCartao(
+    'Inglês',
+    'What does CAR mean?',
+    'Carro'
+)
+
+criaCartao(
+    'Inglês',
+    'What does POOL mean?',
+    'Picina'
+)
+
+criaCartao(
+    'Inglês',
+    'What does SOCCER mean?',
+    'Futebol'
+)
+
+criaCartao(
+    'Inglês',
+    'What does TREE mean?',
+    'Árvore'
+)
